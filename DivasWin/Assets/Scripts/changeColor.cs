879aca9d4dd89cd5a7c9063e96c03c6ce4ff3749
michhessel <michhessel@gmail.com>
2017-12-21 00:07:45 -0500

    Windows project + movement

---
+++ DivasWin/Assets/Scripts/changeColor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using VRStandardAssets.Common;
+using VRStandardAssets.Utils;
+
+public class changeColor : MonoBehaviour {
+
+	private VRInteractiveItem myInteractiveItem;
+	public GameObject hiddenObject;
+
+
+
+	private void Awake()
+	{
+		myInteractiveItem = GetComponent<VRInteractiveItem>();
+	}
+
+
+	//when this script is enabled, bind the "interactiveItem" event to the function below called HandleDown
+	private void OnEnable ()
+	{
+		myInteractiveItem.OnOver += PointerEnter;
+		myInteractiveItem.OnOut += PointerExit;
+	}
+
+	//when this script is disabled, unbindbind the "interactiveItem" event to the function below called HandleDown
+	private void OnDisable ()
+	{
+		myInteractiveItem.OnOver -= PointerEnter;
+		myInteractiveItem.OnOut -= PointerExit;
+	}
+
+	public void PointerEnter()
+	{
+		GetComponent<Renderer> ().material.color = Color.green;
+		hiddenObject.SetActive (true);
+	}
+
+
+	public void PointerExit()
+	{
+		GetComponent<Renderer> ().material.color = Color.white;
+		hiddenObject.SetActive (false);
+	}
+}
---
+++ DivasWin/Assets/Scripts/moveToHere.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using VRStandardAssets.Common;
+using VRStandardAssets.Utils;
+
+public class moveToHere : MonoBehaviour
+
+{
+	//make a variable that is set to the camera. 
+	public Transform player; 
+	public Transform Target; 
+	private VRInteractiveItem myInteractiveItem;                    
+
+	private void Awake()
+	{
+		myInteractiveItem = GetComponent<VRInteractiveItem>();
+
+
+	}
+
+	private void OnEnable ()
+	{
+		myInteractiveItem.OnClick += PointerClick;
+	}
+
+	private void OnDisable ()
+	{
+		myInteractiveItem.OnClick -= PointerClick;
+	}
+
+
+	private void PointerClick(){
+		//call functions to be called on pointer click here
+		player.transform.position = Target.transform.position; 
+		//player.transform.position = new Vector3 (0,0,0);
+		Debug.Log ("click");
+	}
+
+}
